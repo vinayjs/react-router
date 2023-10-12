d7f9a212d359cbccbbcc206d0119f94acc62efea
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-10-12 19:08:56 +0530

    Handle route navigation using Link

--- src/App.js
+++ src/App.js
@@ -6,11 +6,26 @@ import {Home, About, Contact} from "./component"
 
 function App() {
   return (
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/About'>About</Link>
+        </li>
+        <li>
+          <Link to='/Contact'>Contact</Link>
+        </li>
+      </ul>
+    </nav>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/About' element={<About />} />
       <Route path='/Contact' element={<Contact />} />
     </Routes>
+    </>
   )
 }
 
