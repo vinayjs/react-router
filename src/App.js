3ae1c89b12640850e75a181889791cfd3821b7a6
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-10-12 18:57:07 +0530

    Defined Rooutes and Route paths in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import React  from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import {Home, About, Contact} from "./component"
+
 
 function App() {
-  return 
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Contact' element={<Contact />} />
+    </Routes>
+  )
 }
 
 export default App;
